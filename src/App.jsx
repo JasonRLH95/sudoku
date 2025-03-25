a5f6279952750af0ab7eea2665a2a12bd2e4777e
JasonRLH95 <jason95.rm@gmail.com>
2025-03-25 21:33:18 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Routes, Route, HashRouter} from 'react-router-dom';
 import './App.css'
 import Login from './components/Login';
 import DifficultyPage from './components/DifficultyPage';
@@ -8,20 +8,20 @@ import Page404 from './components/Page404';
 import RepairSite from './components/RepairSite';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  
   const [username, setUsername] = useState("");
   const [lvl, setLvl] = useState(0);
   return (
     <div className='App_container'>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path='/sudoku' element={<RepairSite/>}/>
+          <Route path='/' element={<RepairSite/>}/>
           <Route path='/temp' element={<Login username={username} setUsername={setUsername}/>}/>
           <Route path='/choose_difficulty' element={<DifficultyPage username={username} setLvl={setLvl} lvl={lvl}/>}/>
           <Route path='/sudoku/game' element={<MainGame username={username} lvl={lvl}/>}/>
           <Route path='*' element={<Page404/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
